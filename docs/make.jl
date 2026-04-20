0efca2d80c18988d03cd9a183aa91694d4def807
bavithra <bavithra.govintharajah@rwth-aachen.de>
2026-04-20 10:14:36 +0200

    Setup Literate.jl Closes #9

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,20 @@
 using Qritical
 using Documenter
+using Literate
 
+# Process Literate files
+literate_dir = joinpath(@__DIR__, "src", "literate")
+output_dir = joinpath(@__DIR__, "src", "generated")
+
+# Only process if directory exists
+if isdir(literate_dir)
+    for file in readdir(literate_dir; match=r"\.jl$")
+        input_path = joinpath(literate_dir, file)
+        Literate.markdown(input_path; flavor=Documenter(), documenter=true)
+    end
+end
+
+# Set up doctest 
 DocMeta.setdocmeta!(Qritical, :DocTestSetup, :(using Qritical); recursive=true)
 
 makedocs(;
@@ -12,12 +26,7 @@ makedocs(;
         edit_link="main",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages=["Home" => "index.md", "Tutorials" => ["Example" => "generated/example.md"]],
 )
 
-deploydocs(;
-    repo="github.com/knottyanyon/Qritical.jl",
-    devbranch="main",
-)
+deploydocs(; repo="github.com/knottyanyon/Qritical.jl", devbranch="main")
