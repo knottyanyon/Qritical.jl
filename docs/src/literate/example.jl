0b78742806412f412d495919dd374c71f3374a1e
bavithra <bavithra.govintharajah@rwth-aachen.de>
2026-04-20 10:15:43 +0200

    Add example and tutorial files for Qritical.jl documentation

---
+++ docs/src/literate/example.jl
@@ -0,0 +1,9 @@
+# # My First Example
+# This is a tutorial demonstrating Qritical.jl
+
+using Qritical
+
+# ## Section 1: Basic Usage
+# Here's how to use the package...
+
+x = 42  # This will appear in the documentation
