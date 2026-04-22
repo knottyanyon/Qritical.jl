97376891ff039987a77891188a7455aa053c4ad0
bavithra <bavithra.govintharajah@rwth-aachen.de>
2026-04-22 23:23:45 +0200

    Refactor SVD tutorial: remove unnecessary markdown blocks for clarity

--- docs/src/tutorials/01_SVD/01_SVD.jl
+++ docs/src/tutorials/01_SVD/01_SVD.jl
@@ -1,37 +1,31 @@
 # # Exercise 01
 
-md"""
-## 1. SVD a matrix
+# ## 1. SVD a matrix
 
-Perform an SVD on the matrix A given in Moodle as A.txt and find the Schmidt rank
-needed if singular values below $10^{−3}$ are discarded.
-"""
+# Perform an SVD on the matrix A given in Moodle as A.txt and find the Schmidt rank
+# needed if singular values below $10^{−3}$ are discarded.
 
-md"""
-## 2. SVD a state
-Perform an SVD on the state psi.jls given in Moodle. The format is a tensor of rank 10, dimensions 210 = 1024.
-Find the Schmidt rank needed if singular values below $10^{−6}$ are discarded for:
-"""
+# ## 2. SVD a state
+# Perform an SVD on the state psi.jls given in Moodle. The format is a tensor of rank 10, dimensions 210 = 1024.
+# Find the Schmidt rank needed if singular values below $10^{−6}$ are discarded for:
+
 
 # ### (a) a bipartition of the system after the first site
 
 # ### (b) a bipartition of the system in the middle
 
-md"""
-## 3. SVD an image
 
-Reproduce an SVD based image compression (use the image in Moodle or anything else
-you might like).
+# ## 3. SVD an image
 
-*Hint:* Perform an SVD on each color channel.
-"""
+# Reproduce an SVD based image compression (use the image in Moodle or anything else
+# you might like).
 
-md"""
+# *Hint:* Perform an SVD on each color channel.
+
 ## 4. Contractions
 
-Generate two random matrices $A, B$ each of size $N \times N$ and calculate the product
-$C_{i,j} = A_{i,k} B_{k,j}$,
-"""
+# Generate two random matrices $A, B$ each of size $N \times N$ and calculate the product
+# $C_{i,j} = A_{i,k} B_{k,j}$,
 
 # ### (a) once without using any libraries
 
